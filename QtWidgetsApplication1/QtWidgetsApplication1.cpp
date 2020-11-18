977a6a802649657032d41f2fb42d1ba4e05b7a51
sunbeam <fcb1995@163.com>
2020-11-18 20:24:42 +0800

    update PLIN Driver

--- QtWidgetsApplication1/QtWidgetsApplication1.cpp
+++ QtWidgetsApplication1/QtWidgetsApplication1.cpp
@@ -1,5 +1,16 @@
 #include "QtWidgetsApplication1.h"
 #include "qtimer.h"
+#include <stdlib.h>
+#include <windows.h>
+#include "MsgStruct.h"
+
+MSG_0A_TYPE msg_0x0A;
+MSG_10_TYPE msg_0x10;
+MSG_11_TYPE msg_0x11;
+MSG_19_TYPE msg_0x19;
+MSG_1A_TYPE msg_0x1A;
+
+
 
 #define CRUISE_OFF      0
 #define CRUISE_ON       1
@@ -29,7 +40,207 @@ QtWidgetsApplication1::QtWidgetsApplication1(QWidget *parent)
     distval = 1;
     menuid = 0;
     menustate = true;
+
+    //load dll
+    HWList = new QList<QString>;
+    Display(u8"加载成功");
+    int ret = pd.LoadDLL();
+    LoadDLLState = 0;
+    if (ret == RET_ERR)//加载DLL文件
+    {
+        Display(u8"PLinApi.dll加载失败");
+    }
+    else if (ret == RET_OK)
+    {
+        Display(u8"PLinApi.dll加载成功");
+        LoadDLLState = 1;
+    }
+
+    //timer
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(onTimeOut01()));
 }
+
+QtWidgetsApplication1::~QtWidgetsApplication1(void)
+{
+    pd.DoLINDisconnect();//断开连接
+
+}
+
+void QtWidgetsApplication1::onTimeOut01(void)
+{
+    static uint8_t ScheduleCount=0;
+    uint8_t msgid;
+    BYTE msgbuffer[8];
+    if (pd.isConnect() == RET_ERR)
+    {
+        Display(u8"硬件未连接");
+        timer->stop();
+        return;
+    }
+
+    ReadMsg(&msgid, msgbuffer);
+
+    ProcessMsg(msgid, msgbuffer);
+
+    switch (ScheduleCount)
+    {
+    case 0:
+        TransmitID(0x0A);//
+        break;
+    case 1:
+        TransmitID(0x19);//
+        break;
+    case 2:
+        TransmitID(0x1A);//
+        break;
+    case 3:
+        TransmitID(0x10);//
+        break;
+    case 4:
+        TransmitID(0x11);//
+        break;
+    default:
+        break;
+    }
+    ScheduleCount++;
+    if (ScheduleCount > 4)
+    {
+        ScheduleCount = 0;
+    }
+    
+}
+
+void QtWidgetsApplication1::ProcessMsg(uint8_t msgid, BYTE msgdata[])
+{
+    static uint8_t LeftKeyFlag,RightKeyFlag;
+    uint8_t  flag;
+    switch (msgid)
+    {
+    case 0x0A:
+        break;
+    case 0x10:
+        break;
+    case 0x11:
+        break;
+    case 0x19:
+        for (int i = 0; i < 8; i++)
+        {
+            msg_0x19.BYTE[i] = msgdata[i];
+        }
+        flag = 0;
+        if (msg_0x19.Data.Key_ACC)
+        {
+            if (LeftKeyFlag == 0)
+            {
+                ui.cbCruise->clicked();
+                LeftKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (msg_0x19.Data.Key_DIST)
+        {
+            if (LeftKeyFlag == 0)
+            {
+                ui.cbDIST->clicked();
+                LeftKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (msg_0x19.Data.Key_LIM)
+        {
+            if (LeftKeyFlag == 0)
+            {
+                ui.cbLIM->clicked();
+                LeftKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (msg_0x19.Data.Key_RES)
+        {
+            if (LeftKeyFlag == 0)
+            {
+                ui.cbRES->clicked();
+                LeftKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (msg_0x19.Data.Key_SET)
+        {
+            if (LeftKeyFlag == 0)
+            {
+                ui.cbSET->clicked();
+                LeftKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (flag == 0)
+        {
+            LeftKeyFlag = 0;
+        }
+
+        break;
+    case 0x1A:
+        for (int i = 0; i < 8; i++)
+        {
+            msg_0x1A.BYTE[i] = msgdata[i];
+        }
+        flag = 0;
+        if (msg_0x1A.Data.Key_Menu)
+        {
+            if (RightKeyFlag == 0)
+            {
+                ui.cbReturn->clicked();
+                RightKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (msg_0x1A.Data.Key_OK)
+        {
+            if (RightKeyFlag == 0)
+            {
+                ui.cbOK->clicked();
+                RightKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (msg_0x1A.Data.Key_SeekPlus)
+        {
+            if (RightKeyFlag == 0)
+            {
+                ui.cbSeekPlus->clicked();
+                RightKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (msg_0x1A.Data.Key_SeekReduce)
+        {
+            if (RightKeyFlag == 0)
+            {
+                ui.cbSeekMinus->clicked();
+                RightKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (msg_0x1A.Data.Key_Speech)
+        {
+            if (RightKeyFlag == 0)
+            {
+                ui.cbPhone->clicked();
+                RightKeyFlag = 1;
+            }
+            flag = 1;
+        }
+        if (flag == 0)
+        {
+            RightKeyFlag = 0;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
 void QtWidgetsApplication1::on_sbEngine_checkedChanged(bool value)
 {
     if (ui.sbEngine->getChecked())
@@ -384,4 +595,415 @@ void QtWidgetsApplication1::on_pbSeekMinus_clicked(void)
 void QtWidgetsApplication1::on_pbSeekPlus_clicked(void)
 {
     on_cbSeekMinus_clicked();
+}
+
+//Connect 
+void QtWidgetsApplication1::Display(string s)
+{
+    ui.textBrowser->append(QString::fromStdString(s));
+
+}
+
+void QtWidgetsApplication1::on_btnFresh_clicked(void)
+{
+    //QList<UINT8> datebuf(256);
+    int ret;
+    ui.cbbSelectChenal->clear();
+    if (LoadDLLState == 0)
+    {
+        Display(u8"未加载dll，请重新启动");
+    }
+    ret = pd.FreshHW(HWList);//刷新硬件
+
+    if (ret == RET_ERR)
+    {
+        ui.cbbSelectChenal->addItem("未找到硬件");
+    }
+    for (int i = 0; i < HWList->count(); i++)
+    {
+        ui.cbbSelectChenal->addItem((*HWList)[i]);
+    }
+}
+
+
+void QtWidgetsApplication1::on_btnConnect_clicked(void)
+{
+    int ret;
+    ret = pd.DoLINConnect(ui.cbbSelectChenal->currentIndex(), 19200);//连接硬件
+    if (ret == RET_OK)
+    {
+        ui.btnStop->setEnabled(TRUE);
+        ui.btnConnect->setEnabled(FALSE);
+        Display(u8"连接成功");
+        timer->start(10);
+    }
+}
+
+void QtWidgetsApplication1::on_btnStop_clicked(void)
+{
+    pd.DoLINDisconnect();//断开连接
+    ui.btnConnect->setEnabled(TRUE);
+    ui.btnStop->setEnabled(FALSE);
+    Display(u8"断开连接");
+    timer->stop();
+}
+
+void QtWidgetsApplication1::on_btnClear_clicked(void)
+{
+    ui.textBrowser->clear();//清空
+}
+
+void QtWidgetsApplication1::TransmitBlock(char* data, int len, int blockid)
+{
+	BYTE temp[8] = { 0x21,0x10,len + 2,0x36,blockid,0,0,0 };
+	int frameid, i = 0;
+	temp[5] = data[i++];
+	temp[6] = data[i++];
+	temp[7] = data[i++];
+	Write3C(temp);
+	Sleep(10);
+	len -= 3;
+	frameid = 0x21;
+	while (len > 6)
+	{
+		temp[1] = frameid;
+		temp[2] = data[i++];
+		temp[3] = data[i++];
+		temp[4] = data[i++];
+		temp[5] = data[i++];
+		temp[6] = data[i++];
+		temp[7] = data[i++];
+		Write3C(temp);
+		Sleep(10);
+		if (frameid < 0x2F)frameid++;
+		else frameid = 0x20;
+		len -= 6;
+	}
+
+	temp[1] = frameid;
+	int j;
+	for (j = 2; len > 0; len--, j++)
+	{
+		temp[j] = data[i++];
+	}
+	for (; j < 8; j++)
+	{
+		temp[j] = 0xff;
+	}
+	Write3C(temp);
+	Sleep(10);
+}
+
+void QtWidgetsApplication1::Write3C(BYTE* buf)
+{
+	TLINMsg msg;
+	if (pd.isConnect() == RET_ERR)
+	{
+		Display(u8"硬件未连接");
+		return;
+	}
+	msg.FrameId = 0x3C;
+	msg.Direction = dirPublisher;
+	msg.Length = 8;
+	msg.ChecksumType = cstClassic;
+	for (int i = 0; i < 8; i++)
+		msg.Data[i] = buf[i];
+	pd.CalculateChecksum(&msg);
+	pd.Write(pd.m_hClient, pd.m_hHW, &msg);
+}
+
+void QtWidgetsApplication1::TransmitID(int id)
+{
+	TLINMsg msg;
+	if (pd.isConnect() == RET_ERR)
+	{
+		Display(u8"硬件未连接");
+		return;
+	}
+	msg.FrameId = CalculatePID(id);
+	msg.Direction = dirSubscriberAutoLength;//dirPublisher;// 
+	msg.Length = 8;
+	msg.ChecksumType = cstEnhanced;
+	for (int i = 0; i < 8; i++)
+		msg.Data[i] = 0;
+	pd.CalculateChecksum(&msg);
+	pd.Write(pd.m_hClient, pd.m_hHW, &msg);
+}
+
+void QtWidgetsApplication1::TransmitID(int id, BYTE* data)
+{
+	TLINMsg msg;
+	if (pd.isConnect() == RET_ERR)
+	{
+		Display(u8"硬件未连接");
+		return;
+	}
+	msg.FrameId = CalculatePID(id);
+	msg.Direction = dirPublisher;// 
+	msg.Length = 8;
+	msg.ChecksumType = cstEnhanced;
+	for (int i = 0; i < 8; i++)
+		msg.Data[i] = data[i];
+	pd.CalculateChecksum(&msg);
+	pd.Write(pd.m_hClient, pd.m_hHW, &msg);
+}
+
+int QtWidgetsApplication1::CalculatePID(int ID)
+{
+	int P0, P1;
+	P0 = (ID & 0x01) ^ ((ID & 0x02) >> 1) ^ ((ID & 0x04) >> 2) ^ ((ID & 0x10) >> 4);
+	P1 = (((ID & 0x02) >> 1) ^ ((ID & 0x08) >> 3) ^ ((ID & 0x10) >> 4) ^ ((ID & 0x20) >> 5));
+	P1 = P1 ^ 0x01;
+	ID = ID | (P0 << 6) | (P1 << 7);
+
+	return ID;
+}
+
+void QtWidgetsApplication1::Transmit3DHead(void)
+{
+	TLINMsg msg;
+	if (pd.isConnect() == RET_ERR)
+	{
+		Display(u8"硬件未连接");
+		return;
+	}
+	msg.FrameId = 0x7D;
+	msg.Direction = dirSubscriber;
+	msg.Length = 8;
+	msg.ChecksumType = cstClassic;
+	for (int i = 0; i < 8; i++)
+		msg.Data[i] = 0;
+	pd.CalculateChecksum(&msg);
+	pd.Write(pd.m_hClient, pd.m_hHW, &msg);
+}
+
+void QtWidgetsApplication1::Read3D(void)
+{
+	TLINRcvMsg RcvMsg;
+	BYTE temp[8], buffer[60] = { 0 }, * pbuffer;
+	Transmit3DHead();
+	Sleep(10);
+	temp[0] = 0x00;
+	ReadMsg(temp);
+	pbuffer = buffer;
+	while (temp[0] == 0x21)
+	{
+		if (temp[1] == 0x10)
+		{
+			putdata(temp + 2, pbuffer, 6);
+			pbuffer += 6;
+		}
+		else if (temp[1] >= 0x20)
+		{
+			putdata(temp + 2, pbuffer, 6);
+			pbuffer += 6;
+		}
+		else if (temp[1] < 7)
+		{
+			putdata(temp + 1, buffer, temp[1] + 1);
+
+			break;
+		}
+
+
+		Transmit3DHead();
+		Sleep(10);
+		temp[0] = 0x00;
+		ReadMsg(temp);
+	}
+	ProcessDiag(buffer);
+}
+void QtWidgetsApplication1::Read3D(BYTE NAD)
+{
+	TLINRcvMsg RcvMsg;
+	BYTE temp[8], buffer[60] = { 0 }, * pbuffer;
+	Transmit3DHead();
+	Sleep(10);
+	temp[0] = 0x00;
+	ReadMsg(temp);
+	pbuffer = buffer;
+	while (temp[0] == NAD && NAD != 0)
+	{
+		if (temp[1] == 0x10)
+		{
+			putdata(temp + 2, pbuffer, 6);
+			pbuffer += 6;
+		}
+		else if (temp[1] >= 0x20)
+		{
+			putdata(temp + 2, pbuffer, 6);
+			pbuffer += 6;
+		}
+		else if (temp[1] < 7)
+		{
+			putdata(temp + 1, buffer, temp[1] + 1);
+
+			break;
+		}
+
+
+		Transmit3DHead();
+		Sleep(10);
+		temp[0] = 0x00;
+		ReadMsg(temp);
+	}
+	ProcessDiag(buffer);
+}
+
+int QtWidgetsApplication1::Wait3D(BYTE* data, int times)
+{
+	while (times-- > 0)
+	{
+		Transmit3DHead();
+		Sleep(10);
+		data[0] = 0;
+		ReadMsg(data);
+		if (data[0] == 0x21)
+			return 1;
+	}
+	return 0;//超时
+
+}
+
+void QtWidgetsApplication1::ProcessDiag(BYTE* buffer)
+{
+	int len = buffer[0];
+	int DID = 0;
+	if (len == 0)return;
+	
+}
+
+void QtWidgetsApplication1::putdata(BYTE* src, BYTE* dst, int len)
+{
+	while (len--)
+	{
+		*dst++ = *src++;
+	}
+}
+
+void QtWidgetsApplication1::putdata(QByteArray src, int start, BYTE* dst, int len)
+{
+	while (len--)
+	{
+		*dst++ = src[start++];
+	}
+}
+
+
+void QtWidgetsApplication1::ReadMsg(void)
+{
+	TLINRcvMsg RcvMsg;
+	TLINError IsEmpty = errOK;
+	if (pd.isConnect() == RET_ERR)
+	{
+		Display(u8"硬件未连接");
+		return;
+	}
+
+	while (!(IsEmpty & errRcvQueueEmpty))
+	{
+		IsEmpty = pd.Read(pd.m_hClient, &RcvMsg);
+		if (IsEmpty != errOK)
+			continue;
+		if (RcvMsg.Type != mstStandard)
+			continue;
+		if (RcvMsg.ErrorFlags != MSG_ERR_OK)
+			continue;
+		//if (RcvMsg.FrameId != 0x3C && RcvMsg.FrameId != 0x7D)
+			//continue;
+		QString str = "0x";
+		str.append(QString::number(RcvMsg.FrameId & 0x3f, 16));
+		str.append(":");
+		for (int i = 0; i < RcvMsg.Length; i++)
+		{
+			//str.append(QString::number(RcvMsg.Data[i], 16));
+			str.append("0x");
+			str.append(QString("%1").arg(RcvMsg.Data[i], 2, 16, QChar('0')).toUpper());
+			str.append(" ");
+		}
+		//QString strtemp(str.toUpper());
+		ui.textBrowser->append(str);
+
+	}
+}
+
+void QtWidgetsApplication1::ReadMsg(BYTE* data)
+{
+	TLINRcvMsg RcvMsg;
+	TLINError IsEmpty = errOK;
+	if (pd.isConnect() == RET_ERR)
+	{
+		Display(u8"硬件未连接");
+		return;
+	}
+
+	while (!(IsEmpty & errRcvQueueEmpty))
+	{
+		IsEmpty = pd.Read(pd.m_hClient, &RcvMsg);
+		if (IsEmpty != errOK)
+			continue;
+		if (RcvMsg.Type != mstStandard)
+			continue;
+		if (RcvMsg.ErrorFlags != MSG_ERR_OK)
+			continue;
+		//if (RcvMsg.FrameId != 0x3C && RcvMsg.FrameId != 0x7D)
+			//continue;
+		QString str = "0x";
+		str.append(QString::number(RcvMsg.FrameId & 0x3f, 16).toUpper());
+		str.append(":");
+		for (int i = 0; i < RcvMsg.Length; i++)
+		{
+			//str.append(QString::number(RcvMsg.Data[i], 16));
+			str.append("0x");
+			str.append(QString("%1").arg(RcvMsg.Data[i], 2, 16, QChar('0')).toUpper());
+
+			str.append(" ");
+			if (RcvMsg.FrameId == 0x7D)
+				data[i] = RcvMsg.Data[i];
+		}
+		//QString strtemp(str.toUpper());
+		ui.textBrowser->append(str);
+
+	}
+}
+
+void QtWidgetsApplication1::ReadMsg(uint8_t *id, BYTE* data)
+{
+    TLINRcvMsg RcvMsg;
+    TLINError IsEmpty = errOK;
+    if (pd.isConnect() == RET_ERR)
+    {
+        Display(u8"硬件未连接");
+        return;
+    }
+
+    while (!(IsEmpty & errRcvQueueEmpty))
+    {
+        IsEmpty = pd.Read(pd.m_hClient, &RcvMsg);
+        if (IsEmpty != errOK)
+            continue;
+        if (RcvMsg.Type != mstStandard)
+            continue;
+        if (RcvMsg.ErrorFlags != MSG_ERR_OK)
+            continue;
+        //if (RcvMsg.FrameId != 0x3C && RcvMsg.FrameId != 0x7D)
+            //continue;
+        QString str = "0x";
+        str.append(QString::number(RcvMsg.FrameId & 0x3f, 16).toUpper());
+        str.append(":");
+        for (int i = 0; i < RcvMsg.Length; i++)
+        {
+            //str.append(QString::number(RcvMsg.Data[i], 16));
+            str.append("0x");
+            str.append(QString("%1").arg(RcvMsg.Data[i], 2, 16, QChar('0')).toUpper());
+
+            str.append(" ");
+            //if (RcvMsg.FrameId == *id)
+                data[i] = RcvMsg.Data[i];
+        }
+        *id = RcvMsg.FrameId & 0x3f;
+        //QString strtemp(str.toUpper());
+        ui.textBrowser->append(str);
+
+    }
 }
--- QtWidgetsApplication1/QtWidgetsApplication1.h
+++ QtWidgetsApplication1/QtWidgetsApplication1.h
@@ -2,6 +2,13 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_QtWidgetsApplication1.h"
+#include <string>
+#include "PLinApi.h"
+#include "PLIN_Driver.h"
+
+
+using namespace std;
+
 
 class QtWidgetsApplication1 : public QMainWindow
 {
@@ -51,10 +58,65 @@ private slots:
 
     void on_cbPhone_pressed(void);
 
+    void on_btnFresh_clicked(void);
+
+    void on_btnConnect_clicked(void);
+
+    void on_btnStop_clicked(void);
+
+    void on_btnClear_clicked(void);
+
+    void TransmitBlock(char* data, int len, int blockid);
+
+    void Write3C(BYTE* buf);
+
+    void TransmitID(int id);
+
+    void TransmitID(int id, BYTE* data);
+
+    int CalculatePID(int ID);
+
+    void Transmit3DHead(void);
+
+    void Read3D(void);
+
+    void Read3D(BYTE NAD);
+
+    int Wait3D(BYTE* data, int times);
+
+    void ProcessDiag(BYTE* buffer);
+
+    void putdata(BYTE* src, BYTE* dst, int len);
+
+    void putdata(QByteArray src, int start, BYTE* dst, int len);
+
+    void ReadMsg(void);
+
+    void ReadMsg(BYTE* data);
+
+    void ReadMsg(uint8_t* id, BYTE* data);
+
+
+    void onTimeOut01(void);
+
+    void ProcessMsg(uint8_t msgid, BYTE msgdata[]);
+
+
 public:
     QtWidgetsApplication1(QWidget *parent = Q_NULLPTR);
 
+    ~QtWidgetsApplication1(void);
+    
+    QList<QString>* HWList;
+
 private:
+    uint8_t LoadDLLState;
+    PLIN_DRIVER pd;
+    QTimer* timer;
     Ui::QtWidgetsApplication1 ui;
+
+    void Display(string s);
+
+    
     
 };
